8c45331eaae4bcf06463e06d0f074c6be459c014
Dan Bray <d.bray33@gmail.com>
2025-07-19 15:32:32 -0400

    commented out formhandling module

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ const App = {
       ScrollAnimations,
       TestimonialCarousel,
       FAQAccordion,
-      FormHandling,
+      // FormHandling,
       PhoneFormatting,
       ImageHandling,
       PerformanceOptimization,
